d82a5e613b81376c3e17a6dc22e210467b9fdb1e
Damla Erva Kasal <damlaervakasal3@gmail.com>
2023-01-24 01:24:07 +0300

    updated App.js and edded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+import Form from "./components/Form.js";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const toggleComplete = (i) =>
+    setTodos(
+      todos.map((todo, k) =>
+        k === i ? { ...todo, complete: !todo.complete } : todo
+      )
+    );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        onSubmit={(text) => setTodos([{ text, complete: false }, ...todos])}
+      />
+      <div>
+        {todos.map(({ text, complete }, i) => (
+          <div
+            key={text}
+            onClick={() => toggleComplete(i)}
+            style={{
+              textDecoration: complete ? "line-through" : "",
+              backgroundColor: complete ? "red" : "",
+            }}
+          >
+            {text}
+          </div>
+        ))}
+      </div>
+      <button onClick={() => setTodos([])}>reset</button>
+      <br></br>
+      {/* {text.map((text, index) => (
+        <div key={index}>{text}</div>
+      ))} */}
+      {/* <h6>Yapilanlar </h6>  complete olanları üstü çizilenleri otomatik olarak gelmesini sağlayan kod yaz*/}
+      {/* her girilen todo için index ekleyip todo ların kaçıncı sırada olduğunu belirtecek bir şey ekle */}
     </div>
   );
 }
